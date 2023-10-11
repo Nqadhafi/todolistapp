9dd4bb2e4d0e30385be1a79673215a5cab00e21f
Nqadhafi <qadhafi817@gmail.com>
2023-10-12 02:18:21 +0700

    Adjust CSS

--- index.php
+++ index.php
@@ -1,14 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>To Do List Belajar</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+    <h1>Membuat Aplikasi To Do List</h1>
+    <form action="">
+        <label for="">Kegiatan hari ini</label>
+        <input type="text" name="" id="">
+        <input type="submit" value="Submit">
+    </form>
 
-<?php
-
-function kaliDua($target){
-    $target = $target * 2;
-    return $target;
-    
-}
-
-$angka = 10;
-echo kaliDua($angka);
-echo "<br>" . $angka;
-file_put_contents("coba.txt" , $angka . kaliDua($angka) );
-?>
+    <ul>
+        <li>
+            <input type="checkbox" name="" id="">
+            <label for="">To Do Num 1</label>
+        </li>
+    </ul>
+    </div>
+    </div>
+</body>
+</html>
